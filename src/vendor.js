f169dee4a756485d57be0474f0e27b64c65af170
Isidro Torregrosa Torralba <isistorre95@gmail.com>
2017-07-28 11:35:38 +0200

    initial commit

---
+++ src/vendor.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import * as _angular_common from '@angular/common';
+import * as _angular_common_http from '@angular/common/http';
+import * as _angular_platformBrowser from '@angular/platform-browser';
+import * as _angular_platformBrowserDynamic from '@angular/platform-browser-dynamic'
+import * as _angular_router from '@angular/router';
+import * as _angular_compiler from '@angular/compiler';
+import * as _angular_core from '@angular/core';
+import * as _angular_forms from '@angular/forms';
+
+import 'zone.js/dist/zone';
+import 'reflect-metadata';
+import 'rxjs/Rx';
+
+export default {
+  _angular_common,
+  _angular_common_http,
+  _angular_core,
+  _angular_forms,
+  _angular_platformBrowserDynamic,
+  _angular_platformBrowser,
+  _angular_router
+};
